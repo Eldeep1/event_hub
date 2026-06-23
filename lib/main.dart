55e38a2d80a10dc7a11711e6ce960f12aa888d83
depo <alieldeep111@gmail.com>
2026-06-23 20:57:28 +0300

    wrap it up

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
-import 'package:event_hub/features/splash/view/splash_view.dart';
+import 'package:event_hub/data/data_source/local/shared_preferences.dart';
+import 'package:event_hub/data/repoisitory/splash_repo/splash_repo_imp.dart';
+import 'package:event_hub/domain/repository/splash_repo/splash_repo.dart';
+import 'package:event_hub/presentation/splash/view/splash_view.dart';
+import 'package:event_hub/presentation/splash/view_model/cubit/splash_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+
+  final storage = SharedPreferencesStorage(prefs);
+
+  final splashRepo = SplashRepoImp(preferences: storage);
+
+  runApp(MyApp(splashRepo: splashRepo));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final SplashRepo splashRepo;
 
-  // This widget is the root of your application.
+  const MyApp({super.key, required this.splashRepo});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(      
-      home: const SplashView(),
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => SplashCubit(splashRepo)..checkStartup(),
+        child: const SplashView(),
+      ),
     );
   }
 }
