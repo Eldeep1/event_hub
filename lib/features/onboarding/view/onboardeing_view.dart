7e719ebf77fd7f9ebf6077ac5477854760572630
depo <alieldeep111@gmail.com>
2026-06-23 18:40:20 +0300

    init commit

---
+++ lib/features/onboarding/view/onboardeing_view.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class OnboardeingView extends StatelessWidget {
+  const OnboardeingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
