065cbe418e9f36193a25884e88e60d0e6f03ca9b
depo <alieldeep111@gmail.com>
2026-06-24 15:13:33 +0300

    wrapping it up together

--- lib/presentation/home/view/home_view.dart
+++ lib/presentation/home/view/home_view.dart
@@ -1,3 +1,6 @@
+import 'package:event_hub/presentation/home/view/widgets/header/home_header_view.dart';
+import 'package:event_hub/presentation/home/view/widgets/invite_card/home_invite_friend_card.dart';
+import 'package:event_hub/presentation/home/view/widgets/upcoming_events/home_upcoming_events_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +8,20 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("home")));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //1. Header
+            HomeHeaderView(),
+            //2. Upcoming Events
+            const SizedBox(height: 20),
+            HomeUpcomingEventsView(),
+            const SizedBox(height: 30),
+            HomeInviteFriendCard(),
+          ],
+        ),
+      ),
+    );
   }
 }
